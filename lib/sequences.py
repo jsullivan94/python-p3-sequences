1b111b391a2e589b057dfd76459afb2f23eef0b5
James Sullivan <jsullivan8794@gmail.com>
2023-06-14 12:23:28 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+
+    a, b = 0, 1
+    fibonacci_sequence = []
+
+
+    if length == 0:
+        pass
+    elif length == 1:
+        fibonacci_sequence.append(a)
+    else:
+        while len(fibonacci_sequence) < length:
+            fibonacci_sequence.append(a)
+            a, b = b, a + b
+
+    print(fibonacci_sequence)
+
+
+
+print_fibonacci(2000)
+
+
+
+
+
+
+
+
